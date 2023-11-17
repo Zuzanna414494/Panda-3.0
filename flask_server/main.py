77754509b2442af91609e8303dd8debb2ace7635
Gabi363 <g.zmuda@novitas.com.pl>
2023-11-17 14:16:29 +0100

    logowanie

--- flask_server/main.py
+++ flask_server/main.py
@@ -1,14 +1,94 @@
 # an object of WSGI application
 from flask import *  # Flask, redirect, url_for
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user
+
 app = Flask(__name__)  # Flask constructor
 
+################################################################################
+# Tells flask-sqlalchemy what database to connect to
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+# Enter a secret key
+app.config["SECRET_KEY"] = "ENTER YOUR SECRET KEY"
+# Initialize flask-sqlalchemy extension
+db = SQLAlchemy()
 
-# A decorator used to tell the application
-# which URL is associated function
-@app.route('/')
-def hello():
+# LoginManager is needed for our application
+# to be able to log in and out users
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+# Create user model
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+
+
+# Initialize app with extension
+db.init_app(app)
+# Create database within app context
+
+with app.app_context():
+    db.create_all()
+
+
+# Creates a user loader callback that returns the user object given an id
+@login_manager.user_loader
+def loader_user(user_id):
+    return Users.query.get(user_id)
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    # If the user made a POST request, create a new user
+    if request.method == "POST":
+        user = Users(username=request.form.get("username"),
+                     password=request.form.get("password"))
+        # Add the user to the database
+        db.session.add(user)
+        # Commit the changes made
+        db.session.commit()
+        # Once user account created, redirect them
+        # to login route (created later on)
+        return redirect(url_for("login"))
+    # Renders sign_up template if user made a GET request
+    return render_template("sign_up.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # If a post request was made, find the user by
+    # filtering for the username
+    if request.method == "POST":
+        user = Users.query.filter_by(
+            username=request.form.get("username")).first()
+        # Check if the password entered is the
+        # same as the user's password
+        if user.password == request.form.get("password"):
+            # Use the login_user method to log in the user
+            login_user(user)
+            return redirect(url_for("oceny"))
+    # Redirect the user back to the home
+    # (we'll create the home route in a moment)
     return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+
+@app.route("/")
+def home():
+    # Render home.html on "/" route
+    return render_template("home.html")
+
+
+################################################################################
 
 
 @app.route('/admin')
@@ -32,10 +112,10 @@ def hello_user(name):
         return redirect(url_for('hello_guest', guest=name))
 
 
-@app.route('/login', methods=['POST'])
-def login():
-    user = request.form['name']
-    return redirect(url_for('hello_user', name=user))
+# @app.route('/login', methods=['POST'])
+# def login():
+#     user = request.form['name']
+#     return redirect(url_for('hello_user', name=user))
 
 
 @app.route('/oceny')
@@ -50,9 +130,9 @@ def result():
         return render_template("result.html", result=result)
 
 
-@app.route('/marks')
+@app.route('/grades')
 def oceny():
-    return render_template("marks.html")
+    return render_template("grades.html")
 
 
 @app.route('/plan')
