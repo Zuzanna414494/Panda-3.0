908b1cc728a68ed1aa589967f9a00ea1632cd4c1
Gabi363 <g.zmuda@novitas.com.pl>
2023-12-12 17:27:13 +0100

    skończone dodawanie nowych użytkowników

--- flask_server/website/auth.py
+++ flask_server/website/auth.py
@@ -4,6 +4,7 @@ from flask import *
 from flask_login import login_user, logout_user, login_required, current_user
 from .models import *
 from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
 # from readGrades import readGrades
 
 
@@ -17,39 +18,41 @@ def sign_up():
         user_type = request.form.get("user_type")
         # generate_password_hash()
         new_user = Users(login=request.form.get("login"),
-                     password=request.form.get("password"),
+                     password=generate_password_hash(request.form.get("password")),
                      user_type=user_type,
                      email=request.form.get("email"),
                      phone_nr=request.form.get("phone_nr"),
                      photo=f'{request.form.get("login")}.jpg',
                      logged_in=False)
+        db.session.add(new_user)
+        db.session.commit()
 
+        user_id = new_user.user_id
         if user_type == "student":
-            new_student = Students(
+            new_student = Students(student_id=user_id,
                                    name=request.form.get("name"),
                                    surname=request.form.get("surname"),
                                    gradebook_nr=request.form.get("gradebook_nr"),
                                    class_name=request.form.get("class_name"),
                                    date_of_birth=request.form.get("date_of_birth"),
                                    place_of_birth=request.form.get("place_of_birth"),
-                                   address=request.form.get("address"),)
+                                   address=request.form.get("address"))
             db.session.add(new_student)
 
         elif user_type == "parent":
-            new_parent = Parents(
+            new_parent = Parents(parent_id=user_id,
                                  name=request.form.get("name"),
                                  surname=request.form.get("surname"),
                                  student_id=request.form.get("student_id"))
             db.session.add(new_parent)
         elif user_type == "teacher":
-            new_teacher = Teachers(
+            new_teacher = Teachers(teacher_id=user_id,
                                    name=request.form.get("name"),
                                    surname=request.form.get("surname"),
-                                   classrom_nr=request.form.get("classrom_nr"),
+                                   classroom_nr=request.form.get("classroom_nr"),
                                    description=request.form.get("description"))
             db.session.add(new_teacher)
 
-        db.session.add(new_user)
         db.session.commit()
 
         flash('Account created!', category='success')
@@ -103,7 +106,8 @@ def login():
                 session['surname'] = student.surname
                 session['gradebook_nr'] = student.gradebook_nr
                 session['class_name'] = student.class_name
-                session['date_of_birth'] = student.date_of_birth
+                date = student.date_of_birth
+                session['date_of_birth'] = date.strftime("%d %B %Y")
                 session['place_of_birth'] = student.place_of_birth
                 session['address'] = student.address
 
