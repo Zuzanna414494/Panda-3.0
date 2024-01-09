2058ddab38ab21be900e5204b9686a5a593bc9a5
Gabi363 <g.zmuda@novitas.com.pl>
2024-01-09 16:05:35 +0100

    sprawdzanie, czy poprawnie wprowadza się dane przy rejestracji

--- flask_server/website/auth.py
+++ flask_server/website/auth.py
@@ -13,57 +13,115 @@ auth = Blueprint('auth', __name__)
 # żeby dostać się na stronę, trzeba być zalogowanym użytkownikiem
 @login_required
 def sign_up():
+
+    # do sprawdzania, czy był błąd podczas rejestracji
+    error = False
+
     if request.method == "POST":
-        # pobranie rodzaju użytkownika
+        # pobranie danych o użytkowniku
         user_type = request.form.get("user_type")
+        login_ = request.form.get("login")
+        password = request.form.get("password")
+        email = request.form.get("email")
+        phone_nr = request.form.get("phone_nr")
 
-        # stworzenie modelu nowego użytkownika i dodanie go do bazy danych
-        new_user = Users(login=request.form.get("login"),
-                     password=generate_password_hash(request.form.get("password")),
-                     user_type=user_type,
-                     email=request.form.get("email"),
-                     phone_nr=request.form.get("phone_nr"),
-                     photo=f'{request.form.get("login")}.jpg',
-                     logged_in=False)
-        db.session.add(new_user)
-        db.session.commit()
-        user_id = new_user.user_id
+        # sprawdzenie, czy wprowadzono dane do modelu User
+        if not (user_type and login_ and password and email and phone_nr) or user_type not in ('student', 'teacher', 'parent'):
+            # jeśli nie, to komunikat i przekierowanie na stronę do rejestracji
+            flash('Error!', category='error')
+            error = True
+            return redirect(url_for('auth.sign_up'))
 
-        # jeśli użytkownik jest uczniem, to stworzenie modelu nowego ucznia i dodanie go do bazy danych
-        if user_type == "student":
-            new_student = Students(student_id=user_id,
-                                   name=request.form.get("name"),
-                                   surname=request.form.get("surname"),
-                                   gradebook_nr=request.form.get("gradebook_nr"),
-                                   class_name=request.form.get("class_name"),
-                                   date_of_birth=request.form.get("date_of_birth"),
-                                   place_of_birth=request.form.get("place_of_birth"),
-                                   address=request.form.get("address"))
-            db.session.add(new_student)
+        else:
+            # stworzenie modelu nowego użytkownika i dodanie go do bazy danych
+            new_user = Users(login=login_,
+                         password=generate_password_hash(password),
+                         user_type=user_type,
+                         email=email,
+                         phone_nr=phone_nr,
+                         photo=f'{request.form.get("login")}.jpg',
+                         logged_in=False)
+            db.session.add(new_user)
+            db.session.commit()
+            user_id = new_user.user_id
 
-        # jeśli użytkownik jest rodzicem, to stworzenie modelu nowego rodzica i dodanie go do bazy danych
-        elif user_type == "parent":
-            new_parent = Parents(parent_id=user_id,
-                                 name=request.form.get("name"),
-                                 surname=request.form.get("surname"),
-                                 student_id=request.form.get("student_id"))
-            db.session.add(new_parent)
+            # jeśli użytkownik jest uczniem, to po sprowdzeniu, czy wprowadzono wszystkie dane stworzenie modelu nowego
+            # ucznia i dodanie go do bazy danych
+            if user_type == "student":
 
-        # jeśli użytkownik jest nauczycielem, to stworzenie modelu nowego nauczyciela i dodanie go do bazy danych
-        elif user_type == "teacher":
-            new_teacher = Teachers(teacher_id=user_id,
-                                   name=request.form.get("name"),
-                                   surname=request.form.get("surname"),
-                                   classroom_nr=request.form.get("classroom_nr"),
-                                   description=request.form.get("description"))
-            db.session.add(new_teacher)
+                name = request.form.get("name")
+                surname = request.form.get("surname")
+                gradebook_nr = request.form.get("gradebook_nr")
+                class_name = request.form.get("class_name")
+                date_of_birth = request.form.get("date_of_birth")
+                place_of_birth = request.form.get("place_of_birth")
+                address = request.form.get("address")
 
-        db.session.commit()
-        # wyświetlenie komunikatu o poprawnym dodaniu nowego użytkownika
-        flash('Account created!', category='success')
+                if not (name and surname and gradebook_nr and class_name and date_of_birth and place_of_birth and address):
+                    flash('Empty place!', category='error')
+                    error = True
+                    return redirect(url_for('auth.sign_up'))
 
-        # powrót na stronę profilową aktualnego użytkownika
-        return redirect(url_for('views.profile'))
+                else:
+                    new_student = Students(student_id=user_id,
+                                           name=name,
+                                           surname=surname,
+                                           gradebook_nr=gradebook_nr,
+                                           class_name=class_name,
+                                           date_of_birth=date_of_birth,
+                                           place_of_birth=place_of_birth,
+                                           address=address)
+                    db.session.add(new_student)
+
+            # jeśli użytkownik jest rodzicem, to po sprawdzeniu, czy wprowadzono dane, stworzenie modelu nowego rodzica
+            # i dodanie go do bazy danych
+            elif user_type == "parent":
+
+                name = request.form.get("name")
+                surname = request.form.get("surname")
+                student_id = request.form.get("student_id")
+
+                if not (name and surname and student_id):
+                    flash('Empty place!', category='error')
+                    error = True
+                    return redirect(url_for('auth.sign_up'))
+
+                else:
+                    new_parent = Parents(parent_id=user_id,
+                                         name=name,
+                                         surname=surname,
+                                         student_id=student_id)
+                    db.session.add(new_parent)
+
+            # jeśli użytkownik jest nauczycielem, to po sprawdzeniu, czy wprowadzono dane, stworzenie modelu nowego
+            # nauczyciela i dodanie go do bazy danych
+            elif user_type == "teacher":
+
+                name = request.form.get("name")
+                surname = request.form.get("surname")
+                classroom_nr = request.form.get("classroom_nr")
+                description = request.form.get("description")
+
+                if not (name and surname and classroom_nr and description):
+                    flash('Empty place!', category='error')
+                    error = True
+                    return redirect(url_for('auth.sign_up'))
+
+                else:
+                    new_teacher = Teachers(teacher_id=user_id,
+                                           name=name,
+                                           surname=surname,
+                                           classroom_nr=classroom_nr,
+                                           description=description)
+                    db.session.add(new_teacher)
+
+            if not error:
+                db.session.commit()
+                # wyświetlenie komunikatu o poprawnym dodaniu nowego użytkownika
+                flash('Account created!', category='success')
+
+            # powrót na stronę profilową aktualnego użytkownika
+            return redirect(url_for('views.profile'))
 
     # uruchomienie strony do rejestracji
     return render_template("sign_up.html", user=current_user)
